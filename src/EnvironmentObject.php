7130182e8f2d1ede29083ed5ed402993d226748c
JanHuang <bboyjanhuang@gmail.com>
2017-04-19 11:41:00 +0800

    add env object

---
+++ src/EnvironmentObject.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      http://www.fast-d.cn/
+ */
+
+namespace FastD\Utils;
+
+
+/**
+ * Class EnvironmentObject
+ * @package FastD\Utils
+ */
+class EnvironmentObject
+{
+    /**
+     * @return bool
+     */
+    public function isCli()
+    {
+        return 'cli' === php_sapi_name();
+    }
+
+    public function isLocal()
+    {
+        return '127.0.0.1' === $this->getLocalIp();
+    }
+
+    /**
+     * @return string
+     */
+    public function getHostName()
+    {
+        return gethostname();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocalIp()
+    {
+        return gethostbyname($this->getHostName());
+    }
+}
